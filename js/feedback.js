9c8b8096c77860774808fc116c6a8e98d089f34f
Akash Sanyal <270023449@col573-l13-06.local>
2019-10-18 18:56:13 +1300

    15 oct from school

--- js/feedback.js
+++ js/feedback.js
@@ -1,6 +1,32 @@
 $(document).ready(function () {
-
   //Initialising the side navbar
   $('.button-collapse').sideNav();
 
+  // Approve Fin Hampton
+  $('.approve').on('click', function (e) {
+    const output = `<a href="#!" class="green-text">
+    <span>&emsp;&ensp;&emsp;&ensp;&emsp;&ensp;</span></a>`;
+    const nameboldfin = `<div class="title finnamebold" style="font-weight: normal;">Fin f Hampton</div>`;
+    document.getElementById("feedbacksomething").innerHTML = output;
+    // document.getElementById("finnamebold").innerHTML = 'nameboldfin';
+    Materialize.toast('Comment Approved', 3000);
+    $(this).parent().remove();
+    e.preventDefault();
+  });
+
+    // Approve Anton
+    $('.approve2').on('click', function (e) {
+      const output = `<a href="#!" class="green-text">
+      <span>&emsp;&ensp;&emsp;&ensp;&emsp;&ensp;</span></a>`;
+      document.getElementById("anothername2").innerHTML = output;
+      Materialize.toast('Comment Approved', 3000);
+      $(this).parent().remove();
+      e.preventDefault();
+    });
+
+  $('.deny').click(function (e) {
+  $(this).parent().remove();
+  Materialize.toast('Comment Denied', 3000);
+  e.preventDefault();
+  });
 });
